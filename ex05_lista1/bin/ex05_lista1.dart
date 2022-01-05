735d6adff0addc7be159a4b70670d01420650455
Alice Soares <71588383+AliceSoaresEng@users.noreply.github.com>
2022-01-04 22:09:57 -0300

    Add files via upload

---
+++ ex05_lista1/bin/ex05_lista1.dart
@@ -0,0 +1,16 @@
+void main() {
+  final valorA = 3;
+  final valorB = 5;
+  
+  print('Valores originais:');
+  print("Valor A: $valorA");
+  print("Valor B: $valorB");
+
+  final valorA1 = valorB;
+  final valorB1 = valorA;
+
+  print('Valores ivertidos:');
+  print("Valor A: $valorA1");
+  print("Valor B: $valorB1");
+
+}
