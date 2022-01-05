4b87ac1aecca1dcfb56f73a16add5783b992523c
Alice Soares <71588383+AliceSoaresEng@users.noreply.github.com>
2022-01-05 19:31:28 -0300

    Add files via upload

---
+++ ex10_lista1/bin/ex10_lista1.dart
@@ -0,0 +1,15 @@
+void main() {
+  var valor  ;
+  //var valor = 23  ;
+  
+if (valor == null){
+  print( "NullPointerException: Erro ao acessar um valor nulo na memória");
+
+}
+else {
+  print("Valor informado: $valor");
+}
+ 
+
+
+}
